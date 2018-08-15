502a1b6897599bf254afb069347bc4ed4576b6bc
Robin Tram <robin.tram@gmail.com>
2018-08-15 23:49:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-function add(a, b) {
+function add(a,b) {
   return (a+b)
 }
 function subtract (a,b) {
   return (a-b)
+}
+function multiple (a,b) {
+  return (a*b)
+}
+function divide (a,b) {
+  return (a/b)
 }
