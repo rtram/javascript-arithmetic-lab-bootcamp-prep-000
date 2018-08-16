4153850be65221982747f06c45b54759fa206f96
Robin Tram <robin.tram@gmail.com>
2018-08-16 00:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function dec(n) {
   return (--n)
 }
 function makeInt(n) {
-  parseInt('n',10)
+  return parseInt('n',10)
 }
 function preserveDecimal (n) {
-  parseFloat('n')
+  return parseFloat('n')
 }
